4dc967462c5eee9c2c604f7ff1ea851996cb4f08
hagiharakotone <hagiharakotone@localhost>
2024-04-04 15:58:47 +0900

    Firstcommit

---
+++ src/main/java/jp/co/example/controller/TopController.java
@@ -0,0 +1,20 @@
+package jp.co.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+//最初のTop.jspの表示からnemu.jspの画面遷移
+
+@Controller
+public class TopController {
+	//http://localhost:8080/でtop.jspを表示
+	@RequestMapping("/")
+	public String showTop() {
+		return "top";
+	}
+
+	@RequestMapping("/menu")
+	public String shownemu() {
+		return "menu";
+	}
+}
